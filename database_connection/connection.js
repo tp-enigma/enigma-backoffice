510321d37ce261eaaab19fea6782e1a1a24e87d5
Jolan Plot <jolan.plot@hotmail.fr>
2020-04-22 13:50:43 +0200

    Update credential

--- database_connection/connection.js
+++ database_connection/connection.js
@@ -1,21 +1,23 @@
 const pg = require('pg');
-
 const config = {
-    host: '<your-db-server-name>.postgres.database.azure.com',
+    //host: '<your-db-server-name>.postgres.database.azure.com',
+    host: '104.42.177.66',
     // Do not hard code your username and password.
     // Consider using Node environment variables.
-    user: '<your-db-username>',     
-    password: '<your-password>',
-    database: '<name-of-database>',
-    port: 1111,
-    ssl: true
+    user: 'enigma',     
+    password: 'enigma',
+    database: 'enigma',
+    port: 5432,
+    ssl: false
 };
 
 const client = new pg.Client(config);
 
+
 client.connect(err => {
     if (err) throw err;
     else {
+        console.log("Test création client")
         queryDatabase();
     }
 });
