96f12413c8f6080c03c47a2b0efac3c53294ca12
Defrasnes <Jessydefrasnes242@hotmail.com>
2020-04-22 15:19:11 +0200

    First commit
    Background + logo

---
+++ enigma-backoffice-front/src/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Logo.css';
+import logo from './logo-enigma-school-slider1.png';
+
+
+function Logo() {
+  return (
+    <div className="Logo">
+        <img src={logo} id="logo" />
+    </div>
+  );
+}
+
+export default Logo;
--- enigma-backoffice-front/src/index.js
+++ enigma-backoffice-front/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Logo from './Logo';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Logo />
   </React.StrictMode>,
   document.getElementById('root')
 );
