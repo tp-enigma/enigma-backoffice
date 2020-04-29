cb7ffde42fb6210c2052ee98eec00e6247d38754
Defrasnes <jessydefrasnes242@hotmail.com>
2020-04-29 09:29:05 +0200

    Bug Fix

--- enigma-backoffice-front/src/App.js
+++ enigma-backoffice-front/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AnnuaireComp from './components/AnnuaireComp';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AnnuaireComp from "./components/AnnuaireComp";
 
 function App() {
   return (
